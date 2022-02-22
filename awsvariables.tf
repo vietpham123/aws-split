ebdc43f77f5900a9b2b3ca517eb4612ec9e8a200
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2022-02-22 09:13:03 -0600

    Create awsvariables.tf

---
+++ awsvariables.tf
@@ -0,0 +1,34 @@
+################################################
+# Terraform variables                     
+################################################
+
+variable "access_key" {
+    type = string
+}
+
+variable "secret_key" {
+    type = string
+}
+
+################################################
+# Region to use for services, customer defined 
+################################################
+
+variable "region" {
+    type = string
+    default="<%=customOptions.awsRegion%>"
+}
+
+################################################
+# Terraform variables                          
+################################################
+
+variable "compute_name" {
+    type = string
+    default="<%=customOptions.awsCompute%>"
+}
+
+variable "vpc_name" {
+    type = string
+    default="<%=customOptions.awsVPC%>"
+}
