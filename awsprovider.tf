7db5810ab558c2ffac6b3f0d88eb966284e0c621
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2022-02-22 09:12:43 -0600

    Create awsprovider.tf

---
+++ awsprovider.tf
@@ -0,0 +1,9 @@
+###########################################################
+# AWS provider configuration                              
+###########################################################
+
+provider "aws" {
+    region = var.region
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
